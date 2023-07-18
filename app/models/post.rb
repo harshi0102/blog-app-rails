de84e1bbbfcd2218d1a814dc4b7825c244bf0f41
unknown <harshika0102@gmail.com>
2023-07-18 23:52:24 +0530

    Created databases and its settings

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+# app/models/post.rb
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+end
