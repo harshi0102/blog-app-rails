ee6c08470e5921558ce940b45f266efae744ca29
Harshika Govind <harshika0102@gmail.com>
2023-07-28 19:33:53 +0530

    Add Forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,5 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.order(created_at: :desc)
-    @user_photo = @user.photo.attached? ? @user.photo : 'photo-1508921912186-1d1a45ebb3c1.jpg'
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :photo)
   end
 end
