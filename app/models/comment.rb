3443d4e757d12ca59c7728b695b8780f43214fd4
Harshika Govind <harshika0102@gmail.com>
2023-07-28 09:14:52 +0530

    fixed bugs in code

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :post
-  belongs_to :author, class_name: 'User', foreign_key: :author_id
-
+  belongs_to :post, class_name: 'Post', foreign_key: :post_id
+  belongs_to :user, class_name: 'User', foreign_key: :user_id
 
   after_create :update_post_comments_counter
 
