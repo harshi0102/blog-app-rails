45ddd1aa3fcf185ae337aabb583f2e016ec18b04
Harshika Govind <harshika0102@gmail.com>
2023-07-31 21:54:46 +0530

    Added some code for integeration specs

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,18 @@
 class CommentsController < ApplicationController
-  before_action :set_post, only: %i[new create]
-
   def new
     @comment = Comment.new
+    @post = Post.find(params[:post_id])
   end
 
   def create
+    @post = Post.find(params[:post_id])
     @comment = Comment.new(
       user_id: current_user.id,
-      post_id: @post.id,
+      post_id: params[:post_id],
       text: params[:text]
     )
     return unless @comment.save
 
     redirect_to user_post_path(@comment.user.id, @post.id)
-  end
-
-  private
-
-  def set_post
-    @post = Post.find(params[:post_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,18 @@
 Rails.application.routes.draw do
-  # Root route
   root "users#index"
+  # root 'pages#blog'
 
-  # Users routes
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create]
   end
 
-  # Posts routes
   resources :posts do
     resources :comments, only: [:new, :create]
     resources :likes, only: [:create]
   end
 
-  # Custom route example (if needed)
-  # get "user_posts_path", to: "posts#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Additional routes can be defined here...
-
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
