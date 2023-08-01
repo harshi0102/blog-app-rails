ed47588082482e28a9f15c2892a1f2571db2f694
Harshika Govind <harshika0102@gmail.com>
2023-08-01 22:02:33 +0530

    added code and fixed bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :likes
 
   validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_posts_counter
     update(posts_counter: posts.count)
